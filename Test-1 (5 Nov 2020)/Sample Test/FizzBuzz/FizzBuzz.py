8d41ee5c42135ebc3a9a992d65b29d179fc4100f
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 17:40:35 +0530

    Update FizzBuzz.py

--- Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.py
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.py
@@ -6,16 +6,16 @@ import random
 import re
 import sys
 
-
-
-#
-# Complete the 'fizzBuzz' function below.
-#
-# The function accepts INTEGER n as parameter.
-#
-
 def fizzBuzz(n):
-    # Write your code here
+    for i in range (1,n+1):
+        if i%15==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else :
+            print(i)
 
 if __name__ == '__main__':
     n = int(input().strip())
