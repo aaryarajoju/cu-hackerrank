0f3bcb336a2e45acb8e32977bb3a6c51a95d4194
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 17:36:17 +0530

    Update FizzBuzz.c

--- Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.c
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.c
@@ -17,14 +17,18 @@ int parse_int(char*);
 
 
 
-/*
- * Complete the 'fizzBuzz' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
 void fizzBuzz(int n) {
-
+    for (int i=1; i<=n; i++){
+        if (i%15==0){
+            printf("FizzBuzz\n");
+        } else if (i%3==0){
+            printf("Fizz\n");
+        } else if (i%5==0){
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
 }
 
 int main()
