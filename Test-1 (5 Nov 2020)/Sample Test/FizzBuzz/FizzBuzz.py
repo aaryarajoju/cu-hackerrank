b1ca4cfd4ecd4c373c10df6095a1e23aa96d9f19
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 17:16:14 +0530

    Create FizzBuzz.py

---
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def fizzBuzz(n):
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    fizzBuzz(n)
