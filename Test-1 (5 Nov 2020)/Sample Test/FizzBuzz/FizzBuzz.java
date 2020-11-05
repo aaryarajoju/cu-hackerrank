271554d83e35e3b0edba01a75e6c4f42ac96c1f8
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 17:27:30 +0530

    Create FizzBuzz.java

---
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
+    }
+}
