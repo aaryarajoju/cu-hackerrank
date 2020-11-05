2de66fac2593a556bf911509b5745731bfbb29a6
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 17:22:51 +0530

    Create FizzBuzz.kt

---
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.kt
@@ -0,0 +1,37 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fun fizzBuzz(n: Int): Unit {
+    // Write your code here
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    fizzBuzz(n)
+}
