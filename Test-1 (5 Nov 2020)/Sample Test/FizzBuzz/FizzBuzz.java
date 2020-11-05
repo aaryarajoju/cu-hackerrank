d803acfc8af12e0a36225421795d95038a06f7c4
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 19:10:15 +0530

    Update FizzBuzz.java

--- Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.java
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.java
@@ -14,14 +14,17 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'fizzBuzz' function below.
-     *
-     * The function accepts INTEGER n as parameter.
-     */
-
     public static void fizzBuzz(int n) {
-    // Write your code here
+    for (int i=1; i<=n; i++) {
+            if (i%15==0)                                                 
+                System.out.println("FizzBuzz");
+            else if (i%5==0)     
+                System.out.println("Buzz"); 
+            else if (i%3==0)     
+                System.out.println("Fizz");               
+            else 
+                System.out.println(i);                         
+        }
 
     }
 
