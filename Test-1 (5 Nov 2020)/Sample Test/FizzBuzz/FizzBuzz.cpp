3152d8d87c0cd605a4fcc1cb68b2f2b0db194fb7
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-05 19:26:25 +0530

    Update FizzBuzz.cpp

--- Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.cpp
+++ Test-1 (5 Nov 2020)/Sample Test/FizzBuzz/FizzBuzz.cpp
@@ -6,15 +6,22 @@ string ltrim(const string &);
 string rtrim(const string &);
 
 
-
-/*
- * Complete the 'fizzBuzz' function below.
- *
- * The function accepts INTEGER n as parameter.
- */
-
 void fizzBuzz(int n) {
-
+    for (int i=1; i<=n; i++)
+    {
+        if (i%15 == 0)        
+            cout << ("FizzBuzz\n");    
+         
+        else if ((i%3) == 0)    
+            cout << ("Fizz\n");                 
+ 
+        else if ((i%5) == 0)                       
+            cout << "Buzz\n";                 
+     
+        else            
+            cout << i << endl;                 
+ 
+    }
 }
 
 int main()
