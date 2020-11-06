f64d0749eb56d5097238233353761941158f415a
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-06 12:52:57 +0530

    Create PrimeOrNot.kt

---
+++ Test-1 (5 Nov 2020)/Test/Q3. Prime or Not/PrimeOrNot.kt
@@ -0,0 +1,40 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+
+/*
+ * Complete the 'isPrime' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+fun isPrime(n: Long): Int {
+    // Write your code here
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toLong()
+
+    val result = isPrime(n)
+
+    println(result)
+}
