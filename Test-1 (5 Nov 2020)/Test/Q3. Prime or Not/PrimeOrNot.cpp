c76c2eb6926561c1ad4994d1d7c858c8837be3cc
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-06 12:52:06 +0530

    Create PrimeOrNot.cpp

---
+++ Test-1 (5 Nov 2020)/Test/Q3. Prime or Not/PrimeOrNot.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+/*
+ * Complete the 'isPrime' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+int isPrime(long n) {
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    int result = isPrime(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
