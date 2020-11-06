e0c2bd3db288dc700890368a0c85352b73855886
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-11-06 15:17:46 +0530

    Update AddTwoNum.py

--- Test-1 (5 Nov 2020)/Test/Q2. Adding Two Numbers/AddTwoNum.py
+++ Test-1 (5 Nov 2020)/Test/Q2. Adding Two Numbers/AddTwoNum.py
@@ -6,19 +6,10 @@ import random
 import re
 import sys
 
-
-
-#
-# Complete the 'addNumbers' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. FLOAT a
-#  2. FLOAT b
-#
+import math
 
 def addNumbers(a, b):
-    # Write your code here
+    return(math.floor(a+b))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
